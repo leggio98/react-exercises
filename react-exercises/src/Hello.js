8e704845a575772b865bb9f0fdf5435e535912d6
[Leggio] <[leggio989898@gmail.com]>
2022-07-09 14:52:24 +0200

    Add Hello class

---
+++ react-exercises/src/Hello.js
@@ -0,0 +1,8 @@
+// Write a Hello class components that renders the "Hello, World!" message within an h1 tag.
+import React from "react" ;
+
+export class Hello extends React.Component {
+    render () {
+        return <h1>Hello, World!</h1>
+    }
+}
