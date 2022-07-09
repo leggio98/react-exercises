b09a44932a1dc07ac608985dac57258772aa67eb
[Leggio] <[leggio989898@gmail.com]>
2022-07-09 15:02:09 +0200

    Add root render

---
+++ react-exercises/src/Index.js
@@ -0,0 +1,10 @@
+// Create a new index.js file within the src folder and render the App component using the ReactDOM.render method
+
+import {createRoot} from 'react-dom/client';
+import { App } from './App';
+
+
+const container = document.querySelector('#root');
+const root = createRoot(container);
+
+root.render(<App />);
