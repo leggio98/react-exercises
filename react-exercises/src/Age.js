272fd77632c6e3f3ec2e21d34c1321f410bfa287
[Leggio] <[leggio989898@gmail.com]>
2022-07-09 15:58:11 +0200

    Add age class

---
+++ react-exercises/src/Age.js
@@ -0,0 +1,15 @@
+// Extract the "Your age is " message into a new component called Age and render it within the Welcome component.
+// Pass to the Age component the age prop that Welcome is receiving from the App component.
+
+
+import React from "react";
+
+export class Age extends React.Component{
+    render () {
+        return <p>Your age is {this.props.age}</p>
+    }
+}
+
+Age.defaultProps = {
+    age: "not specifiedddd",
+}
--- react-exercises/src/Welcome.js
+++ react-exercises/src/Welcome.js
@@ -4,13 +4,14 @@
 
 
 import React from "react";
+import { Age } from "./Age";
 
 export class Welcome extends React.Component {
 
     render() {
         return <div>
             <p> Welcome, {this.props.name}!</p>
-            <p> Your age is {this.props.age}</p>
+            <Age  />
         </div>
     }
 }
@@ -18,5 +19,4 @@ export class Welcome extends React.Component {
 
 Welcome.defaultProps = {
     name : "World",
-    age: "not specified",
 }
