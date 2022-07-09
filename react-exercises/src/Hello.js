73799ad474a29fdb495e6454ce40792ad38f085d
[Leggio] <[leggio989898@gmail.com]>
2022-07-09 15:04:30 +0200

    What a beautiful day!

--- react-exercises/src/Hello.js
+++ react-exercises/src/Hello.js
@@ -3,6 +3,9 @@ import React from "react" ;
 
 export class Hello extends React.Component {
     render () {
-        return <h1>Hello, World!</h1>
+        return <div>
+            <h1>Hello, World!</h1>
+            <p>What a beautiful day!</p>
+        </div>
     }
 }
