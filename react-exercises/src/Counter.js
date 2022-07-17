11899dbc46dab265615105a24f17c6d8b4e18454
[Leggio] <[leggio989898@gmail.com]>
2022-07-17 17:40:35 +0200

    Add state-02

--- react-exercises/src/Counter.js
+++ react-exercises/src/Counter.js
@@ -1,12 +1,15 @@
 //Create a Counter class component with an internal state containing a count property, initialized to 0. The Counter
 //component should render the count property within an h1 tag, and the count property should be incremented by 1 every second;
 
+//Modify the Counter component so that the initial value of the counter,
+// the increment interval and the increment amount are passed as props to the component.
+
 import React from "react";
 
 export class Counter extends React.Component {
     
     state = {
-        count: 0,
+        count: this.props.initialValue,
 
     }
 
@@ -16,14 +19,20 @@ export class Counter extends React.Component {
         setInterval(() => {
             this.setState((state) => {
                 return {
-                    count: state.count + 1
+                    count: state.count + (this.props.incrementBy)
                 }
             })
-        }, 1000)
+        }, (this.props.intervalTime))
     }
     render() {
         return (
             <h1>Count: {this.state.count}</h1>
         )
     }
+}
+
+Counter.propsDefault = {
+    intervalTime: 1000,
+    initialValue: 0,
+    incrementBy: 1,
 }
