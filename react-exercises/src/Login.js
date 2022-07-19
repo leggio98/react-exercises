5dff4168e5f78dffc9d776b0ae2484fe63ab9ffd
[Leggio] <[leggio989898@gmail.com]>
2022-07-19 11:24:49 +0200

    Add Exercise Forms-03

--- react-exercises/src/Login.js
+++ react-exercises/src/Login.js
@@ -1,6 +1,11 @@
 //Create a Login component containing three inputs: a username input,
 // a password input and a remember checkbox. All three inputs should be controlled components.
 
+//Add a "login" button to the Login component. This button should be disabled as long as
+// the username and password inputs are empty. When clicked, the event handler attached to
+//  the button should call an onLogin function passed as a prop to the Login component, passing it the state.
+
+
 import React from "react";
 
 export class Login extends React.Component {
@@ -9,7 +14,9 @@ export class Login extends React.Component {
         password: '',
         remember: false,
     }
-
+loginButtonClick = () => {
+    this.props.onLogin(this.state)
+}
     handleChanger = (event) => {
         const value = event.target.value
         const name = event.target.name
@@ -43,6 +50,12 @@ export class Login extends React.Component {
             value = {this.state.remember}
             onChange = {this.handleChanger}
             />
+            <button 
+            data-testid="login"
+            disabled = {!this.state.username || !this.state.password}
+             onClick={this.loginButtonClick}>
+                Login
+            </button>
             </div>
         )
     }
