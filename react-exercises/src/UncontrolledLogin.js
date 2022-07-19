151209601703f8d294cff9e3b70db6a57a97097c
[Leggio] <[leggio989898@gmail.com]>
2022-07-19 12:18:04 +0200

    Add: exercise Forms-06

--- react-exercises/src/UncontrolledLogin.js
+++ react-exercises/src/UncontrolledLogin.js
@@ -6,6 +6,12 @@ import React, {createRef} from "react";
 export class UncontrolledLogin extends React.Component {
     _formRef = createRef()
 
+    state = {
+        loginButton: false
+    }
+    componentDidMount() {
+		this._formRef.current.elements.username.focus()
+	}
     handleFormSubmit = (event) => {
         event.preventDefault()
 
@@ -13,25 +19,27 @@ export class UncontrolledLogin extends React.Component {
         const username = event.target.elements.username.value
         const remember = event.target.elements.remember.checked
 
-        console.log(
-            password,
-            username,
-            remember
-        )
+        this.props.onLogin({username, password, remember})
     }
+    loginButtonEnabled = () => {
+		const username = this._formRef.current.elements.username.value
+		const password = this._formRef.current.elements.password.value
+
+		this.setState({
+			loginButton: !!username && !!password 
+		})
+	}
 
     render () {
         return (
             <div>
                 <h3> My uncontrolled form</h3>
                 <form ref={this._formRef} onSubmit ={this.handleFormSubmit}>
-                    <input name= 'username' defaultValue="Alessandro" />
-                    <input name= 'password' type='password' />
+                    <input name= 'username' defaultValue="Alessandro" onChange={this.loginButtonEnabled}/>
+                    <input name= 'password' type='password' onChange={this.loginButtonEnabled}/>
                     <input name= 'remember' type='checkbox' />
-                    <button type="submit">Login</button>
+                    <button type="submit" disabled={!this.state.loginButton}>Login</button>
                     <button type="reset">Reset</button>
-
-                    <button></button>
 
                 </form>
 
