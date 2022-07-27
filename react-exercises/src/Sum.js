23927ecd17d4371a1d632e543729f445edae2e65
[Leggio] <[leggio989898@gmail.com]>
2022-07-27 11:53:04 +0200

    Exercise function component 3

--- react-exercises/src/Sum.js
+++ react-exercises/src/Sum.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-export function Sum({numbers}) {
+export function Sum({numbers = [1,2,3,4,5]}) {
     return (
         <h1>
            {numbers && <h1>Sum is : {numbers.reduce((totalValue, currentValue) => totalValue += currentValue, 0)} </h1> }
