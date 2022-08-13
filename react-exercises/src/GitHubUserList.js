3d0d169ef98c518a085a3baee93c0c9af9d74993
[Leggio] <[leggio989898@gmail.com]>
2022-08-13 17:02:16 +0200

    Add CustomHooks-01

--- react-exercises/src/GitHubUserList.js
+++ react-exercises/src/GitHubUserList.js
@@ -1,7 +1,8 @@
-// Create a GithubUserList component that maintains an array of usernames, showing a 
+// Create a GithubUserList component that maintains an array of usernames, showing a
 // GithubUser component for each username entered. The usernames should be added to the array using an input field and a button.
 
 import { useState } from "react"
+import { GitHubUser } from "./GitHubUser"
 
 
 
@@ -21,6 +22,9 @@ function inputBinder (e) {
         <div>
             <input name = {userToPush} onChange ={inputBinder}/>
             <button onClick={addUser}> Add user</button>
+            {usersArray.map((user,index) => (
+                <GitHubUser key={user + index} username={user}/>
+            ))}
         </div>
     )
 }
